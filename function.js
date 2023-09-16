81c4334d7bb84cae9e23b69e44f12cd60697627b
WA-BASED <145154292+WA-BASED@users.noreply.github.com>
2023-09-16 00:26:28 +0000

    Lec 6 done

---
+++ function.js
@@ -0,0 +1,68 @@
+// function printMessage(){
+
+//     console.log("hello everyone");
+// }
+
+// printMessage();printMessage();printMessage();printMessage();printMessage();
+
+// function withparameter(c_Name="I AM YOU"){
+//     return c_Name
+// }
+
+// console.log(withparameter("FUCK YOU"));
+// console.log(withparameter());
+
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// var t = sum(2,4);
+
+// console.log(t);
+
+// let sum =0;
+
+// function sumofAllParameters(){
+//     console.log(arguments.length);
+//     for(let i=0; i<arguments.length-1;i++){
+//         console.log(arguments[i]);
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+
+
+// sumofAllParameters(1,3,4,53,6,7,3);
+
+// const t= sumofAllParameters(23,435,2,56,2,5);
+// console.log(t);
+
+
+// const squre= function(x){
+//     return x*x;
+// }
+
+// const squre= (x) => x*x;
+
+
+// console.log(squre(34));
+
+
+
+// (function (x) {
+//     console.log(x*x);
+
+// })(2);
+
+//function express
+
+let varName= function (x,y) {
+    var sum = x+y;
+    return sum;
+};
+
+//calling function
+
+console.log(varName(3,5))
